19dd8a6e4367f291c18625ebe0b90a5faf23bfa3
Prince Suwal <princesuwal@gmail.com>
2021-05-30 00:07:29 -0400

    Rest to MVC with index.html

--- ChitChat/src/main/java/com/psuwal/starter/controller/HomeController.java
+++ ChitChat/src/main/java/com/psuwal/starter/controller/HomeController.java
@@ -2,22 +2,37 @@ package com.psuwal.starter.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import com.psuwal.starter.service.UserService;
 import com.psuwal.starter.entity.User;
-@RestController
+
+@Controller
 public class HomeController {
 
+	@Autowired
 	private UserService userService;
+	
 	@GetMapping("/home")
-	public String homePage() {
-		return "Welcome to home Page";
+	public String homePage(Model model) {
+		List<User> allUser = userService.getAllUsers();
+		
+		model.addAttribute("allUser", allUser);
+		
+		return "index";
 	}
 	
 	@GetMapping("/getAllUsers")
 	public List<User> getAllUsers(){
 		return userService.getAllUsers();
 	}
+	
+	@PostMapping("/addUser")
+	public User addUser(@RequestBody User user) {
+		return userService.addUser(user);
+	}	
 }
--- ChitChat/src/main/java/com/psuwal/starter/repository/UserRepo.java
+++ ChitChat/src/main/java/com/psuwal/starter/repository/UserRepo.java
@@ -1,5 +1,11 @@
 package com.psuwal.starter.repository;
 
-public class UserRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.psuwal.starter.entity.User;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer>{
 
 }
